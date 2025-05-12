365f4a12cc7a92a647629faf25aa997184e51df2
B6800 <159796891+B6800@users.noreply.github.com>
2025-05-12 10:01:22 +0200

    Update todo.h

--- todo.h
+++ todo.h
@@ -1,15 +1,12 @@
-//
-// Created by Asus on 2025-04-27.
-//
-#include <stdio.h>
 
+#include <stdio.h>
 #ifndef TODO_H
 #define TODO_H
 typedef struct Task {
     char description[100];
     int done;
 } Task;
-
+//Todo Manager Methods
 void load_tasks();
 void save_tasks();
 void add_task(const char *desc);
